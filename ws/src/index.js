fe21919a765a8c329b50c683f7250dfe1ad0f266
ddboats <ddboats@users.noreply.github.com>
2020-04-14 20:48:09 -0400

    Move time processing to client

--- ws/src/index.js
+++ ws/src/index.js
@@ -25,9 +25,7 @@ reader.connect();
 
 // Process NSQ messages
 reader.on('message', message => {
-  query = JSON.parse(message.body.toString());
-  query.time = getPrettyTime(query.time);
-  io.sockets.emit(NSQ_TOPIC, JSON.stringify(query));
+  io.sockets.emit(NSQ_TOPIC, message.body.toString());
   message.finish();
 });
 
@@ -40,14 +38,3 @@ io.on('connection', connection => {
 server.listen(80, '0.0.0.0', () => {
   console.log('Listening on *:80');
 });
-
-// Convert the unix time into something that a human can read
-function getPrettyTime(unixTime) {
-  date = new Date(unixTime * 1000);
-  hours = date.getHours();
-  minutes = date.getMinutes();
-  pmam = hours >= 12 ? 'PM' : 'AM';
-  hours = hours > 12 ? hours - 12 : hours;
-  cleanTime = `${hours}:${minutes} ${pmam}`;
-  return cleanTime;
-}
